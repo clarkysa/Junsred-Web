dc6c9bd1f21f1c2155f5c4b9275c226bcc783836
clarky <nicovignale0@gmail.com>
2025-03-30 00:03:54 -0300

    Update navbar.tsx

--- components/layout/navbar/navbar.tsx
+++ components/layout/navbar/navbar.tsx
@@ -12,51 +12,55 @@ import {
 
 import PageButton from '@/components/buttons/page-button';
 import SocialButton from '@/components/buttons/social-button';
+import { MusicPlayer } from '@/components/music-player/music-player';
 
 import styles from './navbar.module.css';
 
 export function Navbar() {
   return (
-    <Box className={styles['navbar']} backgroundColor={'purple.900'}>
-      <Box>
-        <PageButton Icon={BsHouse} link={'/'}>
-          Me
-        </PageButton>
-        <PageButton Icon={BsFolder} link={'/projects'}>
-          Projects
-        </PageButton>
-        <PageButton Icon={BsHammer} link={'/hireme'}>
-          Hire Me
-        </PageButton>
+    <>
+      <MusicPlayer />
+      <Box className={styles['navbar']} backgroundColor={'purple.900'}>
+        <Box>
+          <PageButton Icon={BsHouse} link={'/'}>
+            Me
+          </PageButton>
+          <PageButton Icon={BsFolder} link={'/projects'}>
+            Projects
+          </PageButton>
+          <PageButton Icon={BsHammer} link={'/hireme'}>
+            Hire Me
+          </PageButton>
+        </Box>
+        <Flex className={styles['social']}>
+          <SocialButton
+            Icon={BsDiscord}
+            link="https://discord.gg/gatitos2"
+            scheme="teal"
+          />
+          <SocialButton
+            Icon={BsGithub}
+            link="https://github.com/junsred"
+            scheme="gray"
+          />
+          {/* <SocialButton Icon={BsLinkedin} link="https://twitter.com/sammwy" scheme="linkedin" /> */}
+          <SocialButton
+            Icon={BsTwitch}
+            link="https://twitch.tv/junsosu"
+            scheme="purple"
+          />
+          <SocialButton
+            Icon={BsTwitter}
+            link="https://twitter.com/"
+            scheme="twitter"
+          />
+          <SocialButton
+            Icon={BsYoutube}
+            link="https://youtube.com/c/DracoGhoul"
+            scheme="red"
+          />
+        </Flex>
       </Box>
-      <Flex className={styles['social']}>
-        <SocialButton
-          Icon={BsDiscord}
-          link="https://discord.gg/gatitos2"
-          scheme="teal"
-        />
-        <SocialButton
-          Icon={BsGithub}
-          link="https://github.com/junsred"
-          scheme="gray"
-        />
-        {/* <SocialButton Icon={BsLinkedin} link="https://twitter.com/sammwy" scheme="linkedin" /> */}
-        <SocialButton
-          Icon={BsTwitch}
-          link="https://twitch.tv/junsosu"
-          scheme="purple"
-        />
-        <SocialButton
-          Icon={BsTwitter}
-          link="https://twitter.com/"
-          scheme="twitter"
-        />
-        <SocialButton
-          Icon={BsYoutube}
-          link="https://youtube.com/c/DracoGhoul"
-          scheme="red"
-        />
-      </Flex>
-    </Box>
+    </>
   );
 }
